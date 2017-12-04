655916d0788479b1b9885f943d4f51383d2ce2a0
Kalbir Sohi <kalbir@gmail.com>
2017-12-04 23:38:55 +0000

    Would help if the tests were right
    Got the test case wrong!

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -1,7 +1,11 @@
 defmodule CaptchaTest do
   use ExUnit.Case
 
+  test "No repeats" do
+    assert Captcha.complete(1212) == 0
+  end
+  
   test "Repeating pairs" do
-    assert Captcha.complete(1212) == 3
+    assert Captcha.complete(1122) == 3
   end
 end

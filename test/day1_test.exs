7fd7becb5c31a4c4aadc44889b9c8574c345a5ce
Kalbir Sohi <kalbir@gmail.com>
2017-12-04 23:42:04 +0000

    The full test suite
    Added all of the tests from the exercise.

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -8,4 +8,12 @@ defmodule CaptchaTest do
   test "Repeating pairs" do
     assert Captcha.complete(1122) == 3
   end
+  
+  test "All repeats" do
+    assert Captcha.complete(1111) == 4
+  end
+  
+  test "Repeats at the top and tail" do
+    assert Captcha.complete(91212129) == 9
+  end
 end

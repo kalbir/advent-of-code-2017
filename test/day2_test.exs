16e5bf72c663cac12c126f62c56f9057ce784b07
Kalbir Sohi <kalbir@gmail.com>
2017-12-05 09:42:55 +0000

    See I already changed it
    As we have `Enum.max` and `Enum.min` seemed pointless to test Elixir core functions!

--- test/day2_test.exs
+++ test/day2_test.exs
@@ -1,18 +1,12 @@
 defmodule ChecksumTest do
   use ExUnit.Case
 
-  test "Get the minimums" do
-    assert Checksum.list_minimum(753)  == 3 
-    assert Checksum.list_minimum(2468) == 2 
-    assert Checksum.list_minimum(5195) == 1 
+  test "Difference of max and min" do
+    assert Checksum.difference(753)  == 4
+    assert Checksum.difference(2468) == 6
+    assert Checksum.difference(5195) == 8
   end
-  
-  test "Get the maximums" do
-    assert Checksum.list_maximum(753)  == 7 
-    assert Checksum.list_maximum(2468) == 8 
-    assert Checksum.list_maximum(5195) == 9 
-  end
-  
+
   test "Sum the differences" do
     assert Checksum.complete(753, 2468, 5195)  == 18 
   end
